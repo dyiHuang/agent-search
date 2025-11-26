6fa7d2461b1c1502b9fa1bd40db8de850e2fa4e2
huangdayi <692884520@qq.com>
2025-11-26 15:07:12 +0800

    add arg

--- train_ppo.sh
+++ train_ppo.sh
@@ -10,4 +10,4 @@ NODE_RANK=0
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 torchrun $DISTRIBUTED_ARGS \
-       main_ppo.py
+       main_ppo.py --precision bf16
