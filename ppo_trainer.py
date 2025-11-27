54cb93e4136e2738970496ee0a226c2e4342b9a2
huangdayi <692884520@qq.com>
2025-11-27 15:44:30 +0800

    fix increment

--- ppo_trainer.py
+++ ppo_trainer.py
@@ -475,7 +475,7 @@ class MegatronDeepSpeedPPOTrainer:
                 meta_info=meta_info
             )  # PP+TP下：LIST[batch_size/pp_size, 1, vocab_size/tp_size]
 
-            increment = get_num_microbatches() * self.config.megtron.micro_batch_size * self.config.megtron.data_parallel_size
+            increment = data.batch_size()
 
             self.actor.step(lr_kwargs={'increment': increment})
 
@@ -697,8 +697,7 @@ class MegatronDeepSpeedPPOTrainer:
         for data in dataloader:
             metric_micro_batch = self.critic.forward_backward_batch(batch=data, meta_info=meta_info)
 
-            increment = (get_num_microbatches() *
-                         self.config.megtron.micro_batch_size * self.config.megtron.data_parallel_size)
+            increment = data.batch_size()
 
             self.critic.step(lr_kwargs={'increment': increment})
 
