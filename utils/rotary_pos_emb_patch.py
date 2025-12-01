01edbf9b472526be2cbf450494b03637a8cf96e4
huangdayi <692884520@qq.com>
2025-12-01 23:46:40 +0800

    change core attn

--- utils/rotary_pos_emb_patch.py
+++ utils/rotary_pos_emb_patch.py
@@ -15,15 +15,17 @@ def apply_patch():
 
 def apply_rotary_pos_emb_new(
         t: Tensor,  # [seq_len, batch, num_heads, head_dim]
-        freqs: Tensor,  # [seq_len, 1, 2*head_dim]
+        freqs: Tensor,  # [1, seq_len, 2*head_dim]
         config: TransformerConfig,
         cu_seqlens: Optional[Tensor] = None,
         mscale: float = 1.0,
         cp_group: torch.distributed.ProcessGroup = None,
 ):
     # 按最后一维拆分为dim长度的两部分，返回元组(cos_restored, sin_restored)
-    _freqs = torch.zeros_like(freqs, dtype=freqs.dtype,device=freqs.device)
-    _freqs = _freqs.copy_(freqs)
-    cos_restored, sin_restored = torch.split(_freqs, _freqs.shape[-1] // 2, dim=-1)
+    # _freqs = torch.zeros_like(freqs, dtype=freqs.dtype,device=freqs.device)
+    # _freqs = _freqs.copy_(freqs)
+    cos_restored, sin_restored = torch.split(freqs, freqs.shape[-1] // 2, dim=-1)
+    t = t.transpose(0, 1)
     r1, r2 = apply_rotary_pos_emb(t, t, cos_restored, sin_restored, unsqueeze_dim=2)
+    r1 = r1.transpose(0, 1).contiguous()
     return r1
