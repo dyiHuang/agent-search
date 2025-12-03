01c8c16e064f49362e3b52037d97e9b12d823119
huangdayi <692884520@qq.com>
2025-12-03 23:54:16 +0800

    debug

--- modeling_qwen_megatron.py
+++ modeling_qwen_megatron.py
@@ -1313,8 +1313,9 @@ class Qwen2MegatronModel(MegatronModule):
         ]
         if tokenizer is not None:
             input_ids1 = tokenizer.encode(str_list[0], return_tensors="pt", padding="max_length", max_length=128).to('cuda')
-            input_ids2 = tokenizer.encode(str_list[1], return_tensors="pt", padding="max_length", max_length=128).to('cuda')
-            input_ids = torch.cat((input_ids1, input_ids2), dim=0)
+            # input_ids2 = tokenizer.encode(str_list[1], return_tensors="pt", padding="max_length", max_length=128).to('cuda')
+            # input_ids = torch.cat((input_ids1, input_ids2), dim=0)
+            input_ids = input_ids1
         else:
             # 如果没有tokenizer，使用简单数字
             input_ids = torch.tensor([[1, 2, 3]], device='cuda')
