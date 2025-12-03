d57c93f34dfc30ec484504719dead2de46dd3982
huangdayi <692884520@qq.com>
2025-12-03 23:16:17 +0800

    debug

--- modeling_qwen_megatron.py
+++ modeling_qwen_megatron.py
@@ -1251,7 +1251,7 @@ class Qwen2MegatronModel(MegatronModule):
         current_input = input_ids
         attention_mask = torch.ones_like(input_ids, dtype=torch.bool)
 
-        batch_size, max_sequence_length = input_ids.size(0), num_steps
+        batch_size, max_sequence_length = input_ids.size(0), input_ids.size(1) + num_steps
         inference_context = StaticInferenceContext(
             batch_size, max_sequence_length
         )
