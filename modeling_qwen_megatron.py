82508f496442f9bedf503029558e055bcee01694
huangdayi <692884520@qq.com>
2025-12-08 13:00:07 +0800

    debug

--- modeling_qwen_megatron.py
+++ modeling_qwen_megatron.py
@@ -1425,7 +1425,7 @@ class Qwen2MegatronModel(MegatronModule):
         utils.print_rank_0("\n" + "=" * 50)
         utils.print_rank_0("2. 生成过程采样检查")
         utils.print_rank_0("=" * 50)
-        generated = self.generate(input_ids, max_length=612)
+        generated = self.generate(input_ids, max_length=300)
 
         # 3. 验证最终输出
         if tokenizer is not None:
