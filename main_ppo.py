68583d575f0873ae57f2d924768ce166f1014a44
huangdayi <692884520@qq.com>
2025-12-16 15:06:58 +0800

    add env param

--- main_ppo.py
+++ main_ppo.py
@@ -6,6 +6,25 @@ from deepspeed.constants import CROSS_RANK, CROSS_SIZE
 
 from ppo_trainer import MegatronDeepSpeedPPOTrainer
 
+
+def parse_args():
+    parser = ArgumentParser(description="DeepSpeed distributed training launch"
+                                        " utility that creates multiple distributed"
+                                        " processes on a single node")
+    parser.add_argument("--node_rank",
+                        type=int,
+                        default=0,
+                        help="The rank of the node for multi-node distributed "
+                             "training")
+
+    parser.add_argument("--nnodes",
+                        type=int,
+                        default=0,
+                        help="The nums of the node for multi-node distributed "
+                             "training")
+
+    return parser.parse_args()
+
 @hydra.main(config_path='config', config_name='ppo_megatron_trainer', version_base=None)
 def main(config):
     # print initial config
@@ -25,22 +44,3 @@ def main(config):
 
 if __name__ == '__main__':
     main()
-
-
-def parse_args():
-    parser = ArgumentParser(description="DeepSpeed distributed training launch"
-                                        " utility that creates multiple distributed"
-                                        " processes on a single node")
-    parser.add_argument("--node_rank",
-                        type=int,
-                        default=0,
-                        help="The rank of the node for multi-node distributed "
-                             "training")
-
-    parser.add_argument("--nnodes",
-                        type=int,
-                        default=0,
-                        help="The nums of the node for multi-node distributed "
-                             "training")
-
-    return parser.parse_args()
