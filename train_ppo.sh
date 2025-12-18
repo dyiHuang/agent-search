fb04c220e737af0eb9a6918a25e56e830f3b66f4
huangdayi <692884520@qq.com>
2025-12-18 15:11:11 +0800

    revert

--- train_ppo.sh
+++ train_ppo.sh
@@ -2,7 +2,7 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 export ARK_API_KEY=    # for doubao api
 
-GPUS_PER_NODE=8
+GPUS_PER_NODE=4
 MASTER_ADDR=localhost
 MASTER_PORT=6000
 NNODES=1
@@ -10,6 +10,5 @@ NODE_RANK=0
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
 torchrun $DISTRIBUTED_ARGS \
        main_ppo.py
