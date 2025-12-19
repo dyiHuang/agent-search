2490d9635096f1aba09a3e35e6a2899feab1402b
huangdayi <692884520@qq.com>
2025-12-19 15:34:56 +0800

    add ray and actor vllm init

--- ppo_trainer.py
+++ ppo_trainer.py
@@ -147,12 +147,15 @@ class MegatronDeepSpeedPPOTrainer:
         dp_size = world_size // (tp_size * pp_size)
         assert tp_size * pp_size * dp_size == world_size, f"world_size:{world_size}, dp_size:{dp_size}并行度不匹配：TP*PP*DP != WORLD_SIZE"
 
+
+        print(f"torch.distributed.init_process_group start")
         torch.distributed.init_process_group(
             backend="nccl",
             init_method="env://",
             rank=0,
             world_size=world_size
         )
+        print(f"torch.distributed.init_process_group end")
 
         # -------------------------- 步骤 2：DeepSpeed 分布式初始化 --------------------------
         # 替代原生 torch.distributed.init_process_group，创建全局分布式进程组
