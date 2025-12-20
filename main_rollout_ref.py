637f70cf982982ef18218428b7be0b5ff924f2df
huangdayi <692884520@qq.com>
2025-12-21 01:30:38 +0800

    add logits_tensor return;  add sync_model_params method

--- main_rollout_ref.py
+++ main_rollout_ref.py
@@ -1,6 +1,8 @@
 import time
+from typing import Dict, Any
 
 import hydra
+import numpy as np
 import ray
 import torch
 from vllm import LLM, SamplingParams
@@ -50,6 +52,8 @@ def init_ray_and_actor(qwen_model_path):
                 trust_remote_code=True,  # 必设为True！原False是核心问题
                 enable_chunked_prefill=False,  # 禁用chunked prefill（解决长序列兼容）
             )
+            # 获取vllm底层的模型核心（TP分片后的模型）
+            self.vllm_model = self.llm.llm_engine.model_executor.models[0]
 
         def generate_from_tensor(self, input_ids_cpu, sampling_params: SamplingParams):
             """接收cpu张量，返回输出token的cpu张量"""
@@ -71,11 +75,14 @@ def init_ray_and_actor(qwen_model_path):
             )
             # 遍历所有批次的输出，拼接为二维结构（核心修复）
             output_token_list_batch = []
+            batch_logits = []
             for output in outputs:  # 遍历每个批次的生成结果（共batch_size个）
                 token_ids = output.outputs[0].token_ids
+                sample_logits = output.outputs[0].logits
                 # 核心修复：将元组转为列表
                 token_ids = list(token_ids)  # 元组→列表，比如 (300,400) → [300,400]
                 output_token_list_batch.append(token_ids)
+                batch_logits.append(sample_logits)
 
             # 转为二维张量（shape=[batch_size, gen_len]）
             # 注意：若各批次生成长度不同，需padding到相同长度（可选，根据你的业务需求）
@@ -86,10 +93,149 @@ def init_ray_and_actor(qwen_model_path):
                 padded = tokens + [151643] * (max_gen_len - len(tokens))
                 padded_outputs.append(padded)
 
+            pad_value = 0.0
+            padded_logits = []
+            vocab_size = self.llm.llm_engine.model_config.vocab_size  # 获取词表大小
+            for logits in batch_logits:
+                # 计算需要padding的长度
+                pad_len = max_gen_len - len(logits)
+                if pad_len > 0:
+                    # 对vocab_size维度padding（shape=[pad_len, vocab_size]）
+                    padding = np.full((pad_len, vocab_size), pad_value, dtype=np.float32)
+                    padded = np.concatenate([logits, padding], axis=0)
+                else:
+                    padded = logits
+                padded_logits.append(padded)
+
             # 转为2维CPU张量（匹配输入的批次维度）
+            logits_tensor = torch.tensor(padded_logits, dtype=torch.bfloat16)
             output_token_ids = torch.tensor(padded_outputs, dtype=torch.long)  # shape=[batch_size, max_gen_len]
 
-            return output_token_ids
+            return output_token_ids, logits_tensor
+
+        def sync_model_params(self, state_dict: Dict[str, Any], tp_rank, tp_size):
+            full_state_dict = self.vllm_model.named_parameters()
+            for k, v in state_dict.items():
+                if k == "embedding.weight":
+                    local_param = full_state_dict["model.embed_tokens.weight"]
+                    self.param_copy(local_param, v, 0, tp_rank, tp_size)
+                    continue
+
+                if k == "final_norm.weight":
+                    local_param = full_state_dict["model.norm.weight"]
+                    local_param.data.copy_(v.data)
+                    continue
+
+                if k == "lm_head.weight":
+                    local_param = full_state_dict["lm_head.weight"]
+                    local_param.data.copy_(v.data)
+                    continue
+
+                import re
+                match = re.match("layers.{}.self_attention.linear_qkv.weight".replace("{}", r"(\d+)"), k)
+                if match:
+                    layer_idx = match.group(1)
+                    q_param = full_state_dict["model.layers.{}.self_attn.q_proj.weight".format(layer_idx)]
+                    k_param = full_state_dict["model.layers.{}.self_attn.k_prob.weight".format(layer_idx)]
+                    v_param = full_state_dict["model.layers.{}.self_attn.v_proj.weight".format(layer_idx)]
+                    q_size = q_param.size(0) // tp_size
+                    k_size = k_param.size(0) // tp_size
+                    v_size = v_param.size(0) // tp_size
+                    self.param_copy(q_param, v[0:q_size, :], 0, tp_rank, tp_size)
+                    self.param_copy(k_param, v[q_size:q_size+k_size, :], 0, tp_rank, tp_size)
+                    self.param_copy(v_param, v[q_size+k_size:q_size+k_size+v_size, :], 0, tp_rank, tp_size)
+                    continue
+                match = re.match("layers.{}.self_attention.linear_qkv.bias".replace("{}", r"(\d+)"), k)
+                if match:
+                    layer_idx = match.group(1)
+                    q_param = full_state_dict["model.layers.{}.self_attn.q_proj.bias".format(layer_idx)]
+                    k_param = full_state_dict["model.layers.{}.self_attn.k_prob.bias".format(layer_idx)]
+                    v_param = full_state_dict["model.layers.{}.self_attn.v_proj.bias".format(layer_idx)]
+                    q_size = q_param.size(0) // tp_size
+                    k_size = k_param.size(0) // tp_size
+                    v_size = v_param.size(0) // tp_size
+                    self.param_copy(q_param, v[0:q_size], 0, tp_rank, tp_size)
+                    self.param_copy(k_param, v[q_size:q_size+k_size], 0, tp_rank, tp_size)
+                    self.param_copy(v_param, v[q_size+k_size:q_size+k_size+v_size], 0, tp_rank, tp_size)
+                    continue
+
+                match = re.match("layers.{}.self_attention.linear_proj.weight".replace("{}", r"(\d+)"), k)
+                if match:
+                    layer_idx = match.group(1)
+                    o_param = full_state_dict["model.layers.{}.self_attn.o_proj.weight".format(layer_idx)]
+                    self.param_copy(o_param, v, 1, tp_rank, tp_size)
+                    continue
+
+                match = re.match("layers.{}.mlp.linear_fc1.weight".replace("{}", r"(\d+)"), k)
+                if match:
+                    layer_idx = match.group(1)
+                    gate_param = full_state_dict["model.layers.{}.mlp.gate_proj.weight".format(layer_idx)]
+                    up_param = full_state_dict["model.layers.{}.mlp.up_proj.weight".format(layer_idx)]
+                    gate_size = gate_param.size(0) // tp_size
+                    up_size = up_param.size(0) // tp_size
+                    self.param_copy(gate_param, v[0:gate_size, :], 0, tp_rank, tp_size)
+                    self.param_copy(up_param, v[gate_size:gate_size + up_size, :], 0, tp_rank, tp_size)
+                    continue
+
+                match = re.match("layers.{}.mlp.linear_fc2.weight".replace("{}", r"(\d+)"), k)
+                if match:
+                    layer_idx = match.group(1)
+                    down_param = full_state_dict["model.layers.{}.mlp.down_proj.weight".format(layer_idx)]
+                    self.param_copy(down_param, v, 1, tp_rank, tp_size)
+                    continue
+
+                match = re.match("layers.{}.input_layernorm.weight".replace("{}", r"(\d+)"), k)
+                if match:
+                    layer_idx = match.group(1)
+                    local_param = full_state_dict["model.layers.{}.input_layernorm.weight".format(layer_idx)]
+                    local_param.data.copy_(v.data)
+                    continue
+
+                match = re.match("layers.{}.pre_mlp_layernorm.weight".replace("{}", r"(\d+)"), k)
+                if match:
+                    layer_idx = match.group(1)
+                    local_param = full_state_dict["model.layers.{}.post_attention_layernorm.weight".format(layer_idx)]
+                    local_param.data.copy_(v.data)
+                    continue
+
+                print(k)
+
+            return
+
+        @staticmethod
+        def param_copy(local_param, remote_pram, dim, tp_rank, tp_size):
+            split_size = local_param.size(dim) // tp_size
+            start = tp_rank * split_size
+            end = (tp_rank + 1) * split_size
+            if dim == 0:
+                if len(local_param.shape) == 2:
+                    local_param[start:end, :].data.copy_(remote_pram.data)
+                if len(local_param.shape) == 1:
+                    local_param[start:end].data.copy_(remote_pram.data)
+            if dim == 1:
+                local_param[:, start:end].data.copy_(remote_pram.data)
+
+        def _vllm_to_hf_name(self, vllm_name):
+            """vllm参数名 → HF参数名映射（适配Qwen2.5-7B）"""
+            # mapping = {
+            #     "embedding.weight": "model.embed_tokens.weight",
+            #     "layers.{}.self_attention.linear_qkv.weight": "model.layers.{}.self_attn.q_proj.weight",
+            #     "layers.{}.self_attention.linear_qkv.weight": "model.layers.{}.self_attn.q_proj.bias",
+            #     "layers.{}.self_attention.linear_qkv.weight": "model.layers.{}.self_attn.k_prob.weight",
+            #     "layers.{}.self_attention.linear_qkv.weight": "model.layers.{}.self_attn.k_prob.bias",
+            #     "layers.{}.self_attention.linear_qkv.weight": "model.layers.{}.self_attn.v_proj.weight",
+            #     "layers.{}.self_attention.linear_qkv.weight": "model.layers.{}.self_attn.v_proj.bias",
+            #     "layers.{}.self_attention.linear_proj.weight": "model.layers.{}.self_attn.o_proj.weight",
+            #     "layers.{}.mlp.linear_fc1.weight": "model.layers.{}.mlp.gate_proj.weight",
+            #     "layers.{}.mlp.linear_fc1.weight": "model.layers.{}.mlp.up_proj.weight",
+            #     "layers.{}.mlp.linear_fc2.weight": "model.layers.{}.mlp.down_proj.weight",
+            #     "layers.{}.input_layernorm.weight": "model.layers.{}.input_layernorm.weight",
+            #     "layers.{}.pre_mlp_layernorm.weight": "model.layers.{}.post_attention_layernorm.weight",
+            #     "final_norm.weight": "model.norm.weight",
+            #     "lm_head.weight": "lm_head.weight"
+            # }
+            #
+            return vllm_name
 
     # 创建Actor实例，获取全局引用
     vllm_actor_ref = VLLMActor.options(
