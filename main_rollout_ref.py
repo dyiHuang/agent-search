207ce77384fbff91d87248190f0515992e88cc23
huangdayi <692884520@qq.com>
2025-12-21 15:04:40 +0800

    fix vllm_model

--- main_rollout_ref.py
+++ main_rollout_ref.py
@@ -54,8 +54,8 @@ def init_ray_and_actor(qwen_model_path):
                 enable_chunked_prefill=False,  # 禁用chunked prefill（解决长序列兼容）
             )
             # 获取vllm底层的模型核心（TP分片后的模型）
-            print(dir(self.llm.llm_engine))
-            executor = self.llm.llm_engine.executor
+            print(dir(self.llm.llm_engine.model_executor))
+            executor = self.llm.llm_engine.model_executor
             self.vllm_model = executor.model_executor.model
 
         def generate_from_tensor(self, input_ids_cpu, sampling_params: SamplingParams):
