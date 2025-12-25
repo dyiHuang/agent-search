2a902162231c5703b59cc8d6b8f7a80ef0674000
huangdayi <692884520@qq.com>
2025-12-25 13:18:41 +0800

    exp

--- train_ppo.sh
+++ train_ppo.sh
@@ -8,7 +8,7 @@ ray start --address=$RAY_ADDRESS
 GPUS_PER_NODE=4
 MASTER_ADDR=localhost
 MASTER_PORT=6000
-NNODES=1
+NNODES=2
 NODE_RANK=0
 
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
