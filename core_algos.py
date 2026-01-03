2556bf5a183ddcf34ce134423f4fe83d4a1a2c59
huangdayi <692884520@qq.com>
2026-01-03 11:32:03 +0800

    change conf

--- core_algos.py
+++ core_algos.py
@@ -71,7 +71,7 @@ def compute_value_loss(vpreds, returns, values, eos_mask, cliprange_value):
 
 
 def compute_gae_advantage_return(token_level_rewards: torch.Tensor, values: torch.Tensor, eos_mask: torch.Tensor,
-                                 gamma: torch.Tensor = torch.tensor(0.99), lam: torch.Tensor = torch.tensor(0.95)):
+                                 gamma: torch.Tensor = torch.tensor(1.0), lam: torch.Tensor = torch.tensor(1.0)):
     """Adapted from https://github.com/huggingface/trl/blob/main/trl/trainer/ppo_trainer.py
 
     Args:
